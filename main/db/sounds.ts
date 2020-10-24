cde8861ccfbcf4989c0d74ac3207ee1d93da0c92
Jules Youngberg <julesdayoungberg@gmail.com>
2020-10-24 01:55:07 -0700

    update or insert

--- main/db/sounds.ts
+++ main/db/sounds.ts
@@ -4,32 +4,37 @@ import { Sound } from '../../@types';
 
 import dbFactory from './dbFactory';
 
+/**
+ * Sounds Database Interface
+ */
 class Sounds {
     private db: Datastore;
 
     constructor() {
         this.db = dbFactory('sounds.db');
+        this.db.ensureIndex({ fieldName: 'filename', unique: true });
     }
 
-    // TODO validation - ajx? - make sure no duplicates
-    async insert(data: Record<string, any>) {
-        // console.log('sounds db insert: ', data);
-        const result = await this.db.insert(data);
-        return result;
+    async insert(data: Sound) {
+        try {
+            const result = await this.db.insert(data);
+            return result;
+        } catch (error) {
+            // it failed, try to update incase it exists
+            await this.update(data);
+        }
+    }
+
+    async update(data: Sound) {
+        await this.db.update({ filename: data.filename }, data);
     }
 
     async fetch(query: Record<string, any>): Promise<Sound[]> {
-        // console.log('sounds db fetch: ', query);
         const result = await this.db.find(query);
         return result as any;
     }
 
     async clear() {
-        // await this.db.remove({}, { multi: true }, (err, numRemoved) => {
-        //     this.db.loadDatabase((err) => {
-        //         if (err) console.log(err);
-        //     });
-        // });
         await this.db.remove({}, { multi: true });
     }
 }
