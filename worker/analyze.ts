3e773f4cb2f17d00d1fa3a8245fbbfa78c0a8acf
Jules Youngberg <julesdayoungberg@gmail.com>
2020-11-05 14:43:35 -0800

    downsample

--- worker/analyze.ts
+++ worker/analyze.ts
@@ -4,12 +4,10 @@ import { Sound } from '../@types';
 
 import FeatureExtractor from './FeatureExtractor';
 
+const SAMPLE_RATE = 22050;
+
 const extractor = new FeatureExtractor();
 
-/**
- * convert a stereo signal to mono by averaging the two channels
- * @param buffer
- */
 export function toMono(buffer: AudioBuffer) {
     if (buffer.numberOfChannels === 1) {
         return buffer.getChannelData(0);
@@ -24,6 +22,28 @@ export function toMono(buffer: AudioBuffer) {
     throw new Error('unexpected number of channels');
 }
 
+export function downsample(sourceBuffer: AudioBuffer): Promise<Float32Array> {
+    const ctx = new OfflineAudioContext(1, sourceBuffer.duration * 1 * SAMPLE_RATE, SAMPLE_RATE);
+    const buffer = ctx.createBuffer(1, sourceBuffer.length, sourceBuffer.sampleRate);
+
+    buffer.copyFromChannel(toMono(sourceBuffer), 0);
+
+    const source = ctx.createBufferSource();
+    source.buffer = buffer;
+    source.connect(ctx.destination);
+
+    return new Promise((resolve) => {
+        ctx.oncomplete = (e) => {
+            const rendered = e.renderedBuffer;
+            const samples = rendered.getChannelData(0);
+            resolve(samples);
+        };
+
+        ctx.startRendering();
+        source.start(0);
+    });
+}
+
 async function loadSoundFile(filename: string): Promise<Float32Array> {
     let buffer: AudioBuffer | undefined;
     try {
@@ -34,9 +54,9 @@ async function loadSoundFile(filename: string): Promise<Float32Array> {
 
     let samples: Float32Array | undefined;
     try {
-        samples = toMono(buffer);
+        samples = await downsample(buffer);
     } catch (e) {
-        throw new Error(`Error converting '${filename}' to mono: ${e}`);
+        throw new Error(`Error downsampling '${filename}' to mono: ${e}`);
     }
 
     return samples;
