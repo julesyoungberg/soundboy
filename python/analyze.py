8c2471d88fe377f3a01735fd8473987b9e5100ae
Chris Petrone <ctpetrone@gmail.com>
2020-11-25 22:04:48 -0800

    Python updoots (#27)
    * Updated Code sent to Main
    
    * Updated Python Code + Model

--- python/analyze.py
+++ python/analyze.py
@@ -1,42 +1,47 @@
 import os, shutil
-import librosa
+import librosa, librosa.display
+import essentia, essentia.standard
 import numpy as np
-import librosa
-import librosa.display
-import pandas as pd
-from tqdm import tqdm
 import matplotlib.pyplot as plt
 import sklearn
 
 
 
-def get_features(dir):
+def get_features(d):
     sr = 22050
     duration = 5
     try:
-        audio, srate = librosa.load(dir, sr = sr, mono = True, duration = duration, res_type='kaiser_fast')
+        sr = 22050
+        duration = 5
+        audio, srate = librosa.load(d, sr = sr, mono = True, duration = duration, res_type='kaiser_best')
         audio, index = librosa.effects.trim(y = audio, top_db=60, frame_length=2048, hop_length=512)
         nfft = 2048
         hop = 1024
+
 
         # 0 pad to 2048 minimum size
         if audio.size < 2048:
             audio = np.pad(audio, (0, 2048 - audio.size), constant_values=(0, 0))
 
 
-        # Max size limits the length of audio sampls so that all features will have the same length (0 padding)
-        max_size = min(srate * 3, audio.size)
-        mfccs = librosa.feature.mfcc(y=audio, sr=srate, n_mfcc=40, n_fft=nfft, hop_length = hop, power = 2.0)
+        w = essentia.standard.Windowing(type = 'hann')
+        spectrum = essentia.standard.Spectrum()
+        mfcc = essentia.standard.MFCC(numberCoefficients=40, sampleRate=22050, numberBands = 128)
 
-        pad_width = 108 - mfccs.shape[1]
+        mfccs = []
+
+        for frame in essentia.standard.FrameGenerator(audio, frameSize = nfft, hopSize = hop):
+                mfcc_bands, mfcc_coeffs = mfcc(spectrum(w(frame)))
+                mfccs.append(mfcc_coeffs)
+                
+        mfccs = essentia.array(mfccs).T
+        pad_width = 109 - mfccs.shape[1]
         mfccs = np.pad(mfccs, pad_width=((0,0), (0, pad_width)), mode='reflect')
-        
 
-        centroid = librosa.feature.spectral_centroid(y=audio, sr = srate, n_fft = nfft, hop_length = hop)
-        bandwidth = librosa.feature.spectral_centroid(y= audio, sr = srate, n_fft = nfft, hop_length = hop)
-        spectral_contrast = librosa.feature.spectral_contrast(y = audio, sr = srate, n_fft = nfft, hop_length = hop)
+        centroid = librosa.feature.spectral_centroid(y=audio, sr = sr, n_fft = nfft, hop_length = hop)
+        bandwidth = librosa.feature.spectral_centroid(y= audio, sr = sr, n_fft = nfft, hop_length = hop)
         spectral_flatness = librosa.feature.spectral_flatness(y = audio, n_fft = nfft, hop_length = hop)
-        spectral_rolloff = librosa.feature.spectral_rolloff(y = audio, sr = srate, n_fft = nfft, hop_length = hop)
+        spectral_rolloff = librosa.feature.spectral_rolloff(y = audio, sr = sr, n_fft = nfft, hop_length = hop)
         zcr = librosa.feature.zero_crossing_rate(y = audio, hop_length = 1024)
 
 
@@ -45,18 +50,6 @@ def get_features(dir):
             centroid.std(),
             bandwidth.mean(),
             bandwidth.std(),
-            # spectral_contrast[0].mean(),
-            # spectral_contrast[0].std(),
-            # spectral_contrast[1].mean(),
-            # spectral_contrast[1].std(),
-            # spectral_contrast[2].mean(),
-            # spectral_contrast[2].std(),
-            # spectral_contrast[3].mean(),
-            # spectral_contrast[3].std(),
-            # spectral_contrast[4].mean(),
-            # spectral_contrast[4].std(),
-            # spectral_contrast[5].mean(),
-            # spectral_contrast[5].std(),
             spectral_flatness.mean(),
             spectral_flatness.std(),
             spectral_rolloff.mean(),
@@ -68,14 +61,14 @@ def get_features(dir):
         ]
         
         feature_vector = np.array(feature_vector)
-        normalized_feature_vector = (feature_vector - feature_vector.mean(0)) / feature_vector.std(0)
-        return normalized_feature_vector, mfccs
+        #normalized_feature_vector = (feature_vector - feature_vector.mean(0)) / feature_vector.std(0)
+        return feature_vector, mfccs
 
     except Exception as e:
         print("error processing file ", dir, e)
-        os.remove(dir)
+        #os.remove(dir)
         print(f"Deleted file{dir}")
-        return [-1]
+        return [-1], [-1]
 
 
 features = []
@@ -89,18 +82,18 @@ folder = "./Train"
 for root, dirs, files in os.walk(folder, topdown=False):
     for name in files:
         cur_dir = os.path.join(root, name)
-        label = root.split('\\')[-1]
+        label = cur_dir.split('/')[-2]
 
         # # Testing code
         # if (current_label != label):
         #     current_label = label
+        #     print(label)
         #     i = 0
         # if (i < 10):
         #     feature_vector, mfcc = get_features(cur_dir)
         #     i += 1
         #     if len(feature_vector) > 1:
         #         features.append(feature_vector)
-        #         print(i)
         #         # Standardizing mfcc data so coefficient dimension has zero mean and unit variance
         #         mfcc = sklearn.preprocessing.scale(mfcc.astype(float), axis=0)
         #         mfccs.append(mfcc)
@@ -111,7 +104,8 @@ for root, dirs, files in os.walk(folder, topdown=False):
         feature_vector, mfcc = get_features(cur_dir)
         if len(feature_vector) > 1:
             features.append(feature_vector)
-            # Standardizing mfcc data so coefficient dimension has zero mean and unit variance
+            
+            #Standardizing mfcc data so coefficient dimension has zero mean and unit variance
             mfcc = sklearn.preprocessing.scale(mfcc.astype(float), axis=0)
             mfccs.append(mfcc)
             labels.append(label)
@@ -120,6 +114,6 @@ features = np.array(features)
 labels = np.array(labels)
 mfccs = np.array(mfccs)
 
-#np.save('features', features)
-np.save('mfccs_normalized', mfccs)
-#np.save('labels', labels)
+np.save('features', features)
+np.save('mfccs', mfccs)
+np.save('labels', labels)
--- python/test_import.py
+++ python/test_import.py
@@ -1,48 +1,54 @@
 import tensorflow as tf
 import librosa
+import essentia, essentia.standard
 import numpy as np
 import os
+import json
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
-model = tf.keras.models.load_model('saved_models/soundboy_model.h5')
+model = tf.keras.models.load_model('saved_models/soundboy_model_new.h5')
 
 model.summary()
 
-def mfcc(filename):
-    audio, srate = librosa.load(filename, sr = 22050, mono = True, duration = 5, res_type='kaiser_fast')
-    audio, index = librosa.effects.trim(y = audio, top_db=60, frame_length=2048, hop_length=512)
-    nfft = 2048
-    hop = 1024
 
-    # 0 pad to 2048 minimum size
-    if audio.size < 2048:
-        audio = np.pad(audio, (0, 2048 - audio.size), constant_values=(0, 0))
-    mfccs = librosa.feature.mfcc(y=audio, sr=srate, n_mfcc=40, n_fft=nfft, hop_length = hop, power = 2.0)
+labels = np.load('labels.npy')
+labels = np.unique(labels)
 
-    pad_width = 108 - mfccs.shape[1]
-    mfccs = np.pad(mfccs, pad_width=((0,0), (0, pad_width)), mode='reflect')
+print(labels)
+these_labels = []
+pred_labels = []
 
-    return np.array(mfccs)
+mfccs = np.load('x_test_new.npy')
+these_labels = np.load('y_test_new.npy')
+real_labels = []
+print(mfccs[0].shape)
 
-i = 0
-current_label = ""
+from tqdm import tqdm 
 
-folder = "./Train"
-for root, dirs, files in os.walk(folder, topdown=False):
-    for name in files:
-        cur_dir = os.path.join(root, name)
-        label = root.split('\\')[-1]
+for key, mfcc in enumerate(tqdm(mfccs)):
+    mfcc = mfcc.reshape(1, 12, 95, 1)
+    class_prediction = model.predict_classes(mfcc)
+    pred_labels.append(labels[class_prediction])
+    real_labels.append(these_labels[key])
 
-        # Testing code
-        if (current_label != label):
-            current_label = label
-            i = 0
-        if (i < 10):
-            mfccs = mfcc(cur_dir)
-            # Reshape the input so it matches input model was trained on, 40 x 108 x 1 array
-            mfccs = mfccs.reshape(1, 40, 108, 1)
 
-            class_prediction = model.predict_classes(mfccs)
-            probability_matrix = model.predict_proba(mfccs)
+fig = plt.figure()
+ax = fig.add_subplot()
 
-            print(class_prediction[0], probability_matrix[0])
-            i += 1
+
+cm = confusion_matrix(y_true = these_labels, y_pred = pred_labels)
+cax = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+fig.colorbar(cax)
+plt.tight_layout()
+ax.set_xticks(np.arange(10))
+ax.set_yticks(np.arange(10))
+ax.set_xticklabels(labels.tolist(), rotation=45)
+ax.set_yticklabels(labels.tolist())
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+
+plt.show()
+
+
